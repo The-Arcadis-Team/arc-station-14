6cb6436357919ff2d9d24fb8d335d40361619e2c
Eris <eris@erisws.com>
2025-10-17 23:38:43 -0400

    goobcoded theft

--- Content.Client/_Goobstation/Clothing/ToggleableClothingRadialMenu.xaml.cs
+++ Content.Client/_Goobstation/Clothing/ToggleableClothingRadialMenu.xaml.cs
@@ -1,10 +1,20 @@
+// SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Aiden <aiden@djkraz.com>
+// SPDX-FileCopyrightText: 2025 BombasterDS <115770678+BombasterDS@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 BombasterDS <deniskaporoshok@gmail.com>
+// SPDX-FileCopyrightText: 2025 BombasterDS2 <shvalovdenis.workmail@gmail.com>
+// SPDX-FileCopyrightText: 2025 Misandry <mary@thughunt.ing>
+// SPDX-FileCopyrightText: 2025 SX_7 <sn1.test.preria.2002@gmail.com>
+// SPDX-FileCopyrightText: 2025 gus <august.eymann@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using System.Numerics;
 using Content.Client.UserInterface.Controls;
 using Content.Shared.Clothing.Components;
 using Robust.Client.UserInterface;
 using Robust.Client.UserInterface.Controls;
 using Robust.Client.UserInterface.XAML;
-using Robust.Shared.Prototypes;
-using System.Numerics;
 
 namespace Content.Client._Goobstation.Clothing;
 
@@ -30,11 +40,14 @@ public sealed partial class ToggleableClothingRadialMenu : RadialMenu
 
     public void RefreshUI()
     {
-        // Even EmotesMenu has to call this, I'm assuming it's essential.
         var main = FindControl<RadialContainer>("Main");
 
-        if (!_entityManager.TryGetComponent<ToggleableClothingComponent>(Entity, out var clothing)
-            || clothing.Container is not { } clothingContainer)
+        if (!_entityManager.TryGetComponent<ToggleableClothingComponent>(Entity, out var clothing))
+            return;
+
+        var clothingContainer = clothing.Container;
+
+        if (clothingContainer == null)
             return;
 
         foreach (var attached in clothing.ClothingUids)
@@ -72,15 +85,19 @@ public sealed partial class ToggleableClothingRadialMenu : RadialMenu
 
     private void AddToggleableClothingMenuButtonOnClickAction(Control control)
     {
-        if (control is not RadialContainer mainControl)
+        var mainControl = control as RadialContainer;
+
+        if (mainControl == null)
             return;
 
         foreach (var child in mainControl.Children)
         {
-            if (child is not ToggleableClothingRadialMenuButton castChild)
-                continue;
+            var castChild = child as ToggleableClothingRadialMenuButton;
 
-            castChild.OnButtonDown += _ =>
+            if (castChild == null)
+                return;
+
+            castChild.OnPressed += _ =>
             {
                 SendToggleClothingMessageAction?.Invoke(castChild.AttachedClothingId);
                 mainControl.DisposeAllChildren();
